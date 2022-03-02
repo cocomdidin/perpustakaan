22d14de075e96db512f60a54fc4eb096499412fe
cocom <udn.chomr@gmail.com>
2022-03-02 19:50:39 +0700

    membuat modal rak

---
+++ app/Rak.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rak extends Model
+{
+    protected $table = 'rak';
+
+    protected $guarded = [];
+
+    protected $perPage = 10;
+
+}
