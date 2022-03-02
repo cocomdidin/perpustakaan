b7994b48f4044bb73e75f613fce8940dca7eeebb
cocom <udn.chomr@gmail.com>
2022-03-02 19:50:52 +0700

    membuat seeder rak

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
-        // $this->call(BukuSeeder::class);
-        // $this->call(AnggotaSeeder::class);
-        // $this->call(TransaksiSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(BukuSeeder::class);
+        $this->call(AnggotaSeeder::class);
+        $this->call(RakSeeder::class);
     }
 }
