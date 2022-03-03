671d3c7b900f8354a07f0a4c63c13dbafa628559
cocom <udn.chomr@gmail.com>
2022-03-03 23:28:01 +0700

    penyesuaian data dashborad

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,14 +14,33 @@ class DashboardController extends Controller
 {
     public function index()
     {
-    
-             
+
+
         //chart berdasarkan jenis kelamin
-        $jenis_kelamin =  Anggota::groupBy('jenis_kelamin')->select('jenis_kelamin',DB::raw('count(*) as total'))->get();
+        $jenis_kelamin =  Anggota::select('jenis_kelamin',DB::raw('count(transaksi.id) as total'))
+            ->join('transaksi','transaksi.anggota_id','anggota.id')
+            ->whereYear('tgl_pinjam', Carbon::now()->year)
+            ->groupBy('jenis_kelamin')
+            ->get()
+        ;
 
         //chart berdasarkan pinjam dan kembali
-        $transaksi_pinjam = Transaksi::withTrashed()->groupBy('status')->select('status',DB::raw('count(*) as total'))->get();
-    
+        $transaksi_pinjam = Transaksi::withTrashed()
+            ->select(
+                DB::raw('MONTH(tgl_pinjam) bln'),
+                DB::raw('MONTHNAME(tgl_pinjam) bulan'),
+                DB::raw("COUNT(id) as pinjam"),
+                DB::raw("SUM(IF(status='kembali',1,0)) as kembali")
+            )
+            ->whereYear('tgl_pinjam', Carbon::now()->year)
+            ->groupBy('bulan','bln')
+            ->orderBy('bln')
+            ->get()
+        ;
+
+        // $list_bulan = $transaksi_pinjam->pluck('bulan')->toJson();
+        // $list_pinjam = $transaksi_pinjam->pluck('pinjam')->toJson();
+        // $list_kembali = $transaksi_pinjam->pluck('kembali')->toJson();
 
         $data = [
            'buku' =>  Buku::count(),
@@ -30,12 +49,12 @@ class DashboardController extends Controller
            'riwayat' => Transaksi::withTrashed()->count(),
            'jenis_kelamin' => $jenis_kelamin,
            'transaksi_pinjam' => $transaksi_pinjam,
-    
+
         ];
 
         // dd($buku_kategori);
         return view('dashboard',$data);
 
-        
+
     }
 }
