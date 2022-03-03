0692efade155b1213e1ee9bcf3429bfecaa17b5b
cocom <udn.chomr@gmail.com>
2022-03-03 20:52:10 +0700

    revisi tampilan riwayat transaksi

--- resources/views/riwayat/index.blade.php
+++ resources/views/riwayat/index.blade.php
@@ -10,7 +10,11 @@
                     <h3 class="text-left">{{ $title }}</h3>
                   </div>
                   <div class="col text-right">
-                    <a href="{{ route('riwayat.all') }}" class="btn btn-primary">Tampilkan semua</a>
+                    @if($section == 'all')
+                        <a href="{{ route('riwayat.index') }}" class="btn btn-primary">Tampilkan Sebagian</a>
+                    @else
+                        <a href="{{ route('riwayat.all') }}" class="btn btn-primary">Tampilkan semua</a>
+                    @endif
                   </div>
                 </div>
               </div>
@@ -19,48 +23,54 @@
                 <table class="table align-items-center table-flush">
                   <thead class="thead-light">
                     <tr>
-                      <th scope="col">Nama </th>
-                      <th scope="col">No Anggota</th>
-                      <th scope="col">Buku</th>
-                      <th scope="col">Tanggal Pinjam</th>
-                      <th scope="col">Tanggal Kembali</th>
-                      <th scope="col">Status</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">No Anggota</th>
+                        <th scope="col">Kode Transaksi</th>
+                        <th scope="col">Judul Buku</th>
+                        <th scope="col">Tanggal Pinjam</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Terlambat</th>
                     </tr>
                   </thead>
                   <tbody>
                       @foreach ($transaksi as $item)
-
-                      <tr>
-                          <th scope="row">
-                            {{ $item->anggota->nama }}
-                        </th>
-                        <td>
-                            {{ $item->anggota->nim }}
-                        </td>
-                        <td>
-                            {{ $item->buku->judul }}
-                        </td>
-                        <td>
-                            {{ $item->tgl_pinjam }}
-                        </td>
-                        <td>
-                            {{ $item->tgl_kembali }}
-                        </td>
-                        <td>
-                            @if ($item->status == 'kembali')
-                                <span class="badge badge-dot mr-4">
-                                    <i class="bg-success"></i>
-                                {{ $item->status }}
-                                </span>
-                            @else
-                                <span class="badge badge-dot mr-4">
-                                    <i class="bg-danger"></i>
-                                    {{ $item->status }}
-                                </span>
-                            @endif
-
-                        </td>
-                    </tr>
+                        <tr>
+                            <th scope="row">
+                                <div class="media align-items-center">
+                                    <div class="media-body">
+                                        <span class="name mb-0 text-sm">{{ $item->anggota->nama }}</span>
+                                    </div>
+                                </div>
+                            </th>
+                            <td class="budget">
+                                {{ $item->anggota->nim }}
+                            </td>
+                            <td>
+                                <span class="status">{{ $item->kode_transaksi }}</span>
+                            </td>
+                            <td>{{ $item->buku->judul }}</td>
+                            <td>{{ $item->tgl_pinjam }} <span class="text-muted">s/d</span> {{ $item->tgl_max_pinjam }}</td>
+                            <td>
+                                @if ($item->status == 'pinjam')
+                                    <span class="badge badge-dot mr-4">
+                                        <i class="bg-danger"></i>
+                                        <span class="text-capitalize">{{ $item->status }}</span>
+                                    </span>
+                                @else
+                                    <span class="badge badge-dot mr-4">
+                                        <i class="bg-success"></i>
+                                        <small class="text-capitalize">{{ $item->status }}</small>
+                                    </span>
+                                    <div>{{ $item->tgl_kembali }}</div>
+                                @endif
+                            </td>
+                            <td>
+                                <div>{{ $item->terlambat }} hari</div>
+                                @if ($item->terlambat > 0)
+                                    <small class="text-danger">Rp. {{ number_format($item->denda, 0, ',', '.') }}</small>
+                                @endif
+                            </td>
+                        </tr>
                     @endforeach
                 </tbody>
                 </table>
