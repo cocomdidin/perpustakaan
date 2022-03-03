517e1b882ebf2667fdc98ce04f1a7567d5a0f56b
cocom <udn.chomr@gmail.com>
2022-03-03 23:27:40 +0700

    revisi chart

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                         <div class="col">
                             <h5 class="card-title text-uppercase text-muted mb-0">Buku</h5>
                             <span class="h2 font-weight-bold mb-0">{{ $buku }}</span>
-                          
+
                         </div>
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
@@ -54,7 +54,7 @@
                         <div class="col-auto">
                             <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
                                 <i class="ni ni-ruler-pencil"></i>
-                                
+
                             </div>
                         </div>
                     </div>
@@ -84,18 +84,18 @@
         </div>
     </div>
 
-    
+
         <div class="row">
           <div class="col-xl-8">
             <div class="card bg-default">
               <div class="card-header bg-transparent">
                 <div class="row align-items-center">
                   <div class="col">
-                    <h6 class="text-light text-uppercase ls-1 mb-1">Overview</h6>
-                    <h5 class="h3 text-white mb-0">Transaksi</h5>
+                    <h6 class="text-light text-uppercase ls-1 mb-1">Tahun {{ date('Y') }}</h6>
+                    <h5 class="h3 text-white mb-0">Transaksi Pinjam Buku</h5>
                   </div>
                   <div class="col">
-                  
+
                   </div>
                 </div>
               </div>
@@ -103,121 +103,64 @@
                 <!-- Chart -->
                 <div class="chart">
                   <!-- Chart wrapper -->
-            
+
                   <canvas id="myChart" class="chart-canvas chartjs-render-monitor"></canvas>
-                  @php
-                      $trans = '';
-                      $jumlah_trans = null;
 
-                      foreach ($transaksi_pinjam as $item) {
-
-                        //status 
-                        $status = $item->status;
-                        $trans .= "'$status'".",";
-                        //total
-                        $total =  $item->total;
-                        $jumlah_trans .="'$total'".",";
-                        
-                      
-                      }
-                    
-                  @endphp
-                  
                 </div>
-               
+
               </div>
-              
+
             </div>
           </div>
-         
+
           <div class="col-xl-4">
             <div class="card">
               <div class="card-header bg-transparent">
                 <div class="row align-items-center">
                   <div class="col">
-                    <h6 class="text-uppercase text-muted ls-1 mb-1">Overview</h6>
-                    <h5 class="h3 mb-0">Jenis Kelamin</h5>
+                    <h6 class="text-uppercase text-muted ls-1 mb-1">Tahun {{ date('Y') }}</h6>
+                    <h5 class="h3 mb-0">Peminjam Buku</h5>
                   </div>
                 </div>
               </div>
-              
+
               <div class="card-body">
                 <!-- Chart -->
                 <div class="chart">
                   <canvas id="donutChart" class="chart-canvas"></canvas>
-                  @php
-                      $jk = "";
-                      $jumlah = null;
-
-                      foreach ($jenis_kelamin as $item) {
-                        
-                        $jenis = $item->jenis_kelamin;
-                        $jk .= "'$jenis'".",";
-
-                        $tot = $item->total;
-                        $jumlah .= "'$tot'".",";
-                      }
-                  @endphp 
-                 
                 </div>
               </div>
             </div>
           </div>
         </div>
-   
-   
-        
+
+
+
 @endsection
 
 @push('script')
     <script>
-    
-     
-      //setup block
-      const data = {
-        labels: [@php echo $trans; @endphp],
-        datasets: [{
-
-          label: 'jumlah',
-          data: [
-            @php  echo $jumlah_trans; @endphp
-          ],
-          backgroundColor: ['#2dce89','#f5365c'],
-          borderColor: 'rgb(255, 99, 132)'
-        }]
-      };
-      
-  
-        
-      //config block
-        const config ={
-          type: 'bar',
-          data,
-          options: {
-              scales: {
-                  x:{
-                    parsing:false,
-                    type:'time',
-                    time:{
-                     
-                      unit:'day',
+        let chart = new Chart(document.getElementById('myChart'), {
+            type: 'bar',
+            data: {
+                labels: {!! $transaksi_pinjam->pluck('bulan') !!},
+                datasets: [
+                    {
+                        label: 'Pinjam',
+                        data: {!! $transaksi_pinjam->pluck('pinjam') !!},
+                        borderColor: 'rgb(255, 99, 132)'
+                    },
+                    {
+                        label: 'Kembali',
+                        data: {!! $transaksi_pinjam->pluck('kembali') !!},
+                        backgroundColor: '#2dce89',
+                        borderColor: 'rgb(255, 99, 132)'
                     }
-                  },
-                  y: {
-                      beginAtZero: true
-                  }
-                
-              }
-          }
-        }
+                ],
+            }
+        });
 
-      //render /init block
-        const myChart = new Chart(
-            document.getElementById('myChart'),
-            config
-        );
 
-       
 
 
 
@@ -228,10 +171,10 @@
     // Get context with jQuery - using jQuery's .get() method.
     var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
     var donutData        = {
-    labels: [@php echo $jk; @endphp],
+    labels: {!! $jenis_kelamin->pluck('jenis_kelamin') !!},
       datasets: [
         {
-          data:  [@php echo $jumlah; @endphp],
+          data:  {!! $jenis_kelamin->pluck('total') !!},
           backgroundColor : [ '#5e72e4','#f63f55', ],
         }
       ]
@@ -245,7 +188,7 @@
     var donutChart = new Chart(donutChartCanvas, {
       type: 'doughnut',
       data: donutData,
-      options: donutOptions      
+      options: donutOptions
     })
 </script>
 @endpush
