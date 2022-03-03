72afb1b0b2b502db59df8ea0b728e96fb494786d
cocom <udn.chomr@gmail.com>
2022-03-03 20:14:43 +0700

    revis tampilan form kembalikan buku

--- resources/views/transaksi/kembali.blade.php
+++ resources/views/transaksi/kembali.blade.php
@@ -10,45 +10,29 @@
                 </div>
                 <div class="form-group">
                     <label>Buku</label>
-                    {{-- <select name="buku_id" class="form-control" disabled>
-                        <option disabled selected>-- Pilih Buku --</option>
-                        @foreach ($buku as $item)
-                        <option @if($transaksi->buku->id == $item->id) selected @endif value="{{ $item->id }}">{{ $item->judul }}</option>
-                        @endforeach
-                    </select> --}}
                     <input type="text" name="judul" class="form-control" autocomplete="off" value="{{ $transaksi->buku->judul }}" disabled>
                     @error('buku_id')
                         <small class="text-danger">{{ $message }}</small>
                     @enderror
                 </div>
-                {{-- <div class="form-group">
-                    <label>Tanggal Pinjam</label>
-                    <input type="date" name="tgl_pinjam"class="form-control" value="{{ $transaksi->tgl_pinjam }}">
-                    @error('tgl_pinjam')
+                <div class="form-group">
+                    <label>Tgl Max Pinjam</label>
+                    <input type="date" name="tgl_max_pinjam"class="form-control" value="{{ $transaksi->tgl_max_pinjam }}" disabled>
+                    @error('tgl_max_pinjam')
                     <small class="text-danger">{{ $message }}</small>
                     @enderror
-                </div> --}}
+                </div>
                 <div class="form-group">
                     <label>Tanggal Kembali</label>
-                    <input type="date" name="tgl_kembali"class="form-control" value="{{ $transaksi->tgl_kembali }}">
+                    <input type="date" name="tgl_kembali"class="form-control" value="{{ date('Y-m-d') }}" disabled>
                     @error('tgl_kembali')
                     <small class="text-danger">{{ $message }}</small>
                     @enderror
                 </div>
-                {{-- <div class="form-group">
-                    <label for="">Status</label>
-                    <select name="status" class="form-control">
-                        <option disabled selected>-- Pilih Status --</option>
-                        <option @if($transaksi->status == 'pinjam') selected @endif value="pinjam">Pinjam</option>
-                        <option @if($transaksi->status == 'kembali') selected @endif value="kembali">Kembali</option>
-                    </select>
-                </div> --}}
-                @if ($transaksi->ket)
-                    <div class="form-group">
-                        <label>Keterangan</label>
-                        <textarea  name="ket"class="form-control" placeholder="optional">{{ $transaksi->ket }}</textarea>
-                    </div>
-                @endif
+                <div class="form-group">
+                    <label>Keterangan</label>
+                    <textarea  name="ket"class="form-control" placeholder="optional">{{ $transaksi->ket }}</textarea>
+                </div>
 
                 <div class="float-right">
 
