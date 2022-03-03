2960d7abf60568747aa6f524c637128f5a8ee39d
cocom <udn.chomr@gmail.com>
2022-03-03 20:12:46 +0700

    membuat atribute denda dan terlambat

--- app/Transaksi.php
+++ app/Transaksi.php
@@ -2,16 +2,17 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaksi extends Model
 {
     use SoftDeletes;
-    
+
     protected $table = 'transaksi';
 
-    protected $guarded = []; 
+    protected $guarded = [];
 
     public function anggota(){
 
@@ -26,5 +27,15 @@ class Transaksi extends Model
     public function user(){
 
         return $this->belongsTo(User::class);
+    }
+
+    public function getTerlambatAttribute()
+    {
+        return max(Carbon::parse($this->tgl_max_pinjam)->diffInDays(Carbon::parse($this->tgl_kembali), false), 0);
+    }
+
+    public function getDendaAttribute()
+    {
+        return $this->terlambat * 1000;
     }
 }
