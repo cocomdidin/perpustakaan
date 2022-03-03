049e3d8fae4247197d86656ee8eff1be676f0d7c
cocom <udn.chomr@gmail.com>
2022-03-03 20:26:44 +0700

    membuat alert jika ada denda

--- resources/views/transaksi/kembali.blade.php
+++ resources/views/transaksi/kembali.blade.php
@@ -1,43 +1,48 @@
+@if($transaksi->terlambat > 0)
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <span class="alert-text">
+            <strong>Denda Rp. {{ number_format($transaksi->denda, 0, ',', '.') }}</strong><br>
+            Pengembalian buku melebihi tanggal maksimum pinjam selamat {{ $transaksi->terlambat }} hari.
+        </span>
+    </div>
+@endif
 <form action="{{ route('transaksi.kembalikan',$transaksi->id) }}" method="post">
-                @csrf
-                @method('put')
-                <div class="form-group">
-                    <label>No Anggota</label>
-                    <input type="text" placeholder="masukkan no anggota" name="nim"class="form-control" autocomplete="off" value="{{ $transaksi->anggota->nim }}" disabled>
-                    @error('nim')
-                        <small class="text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label>Buku</label>
-                    <input type="text" name="judul" class="form-control" autocomplete="off" value="{{ $transaksi->buku->judul }}" disabled>
-                    @error('buku_id')
-                        <small class="text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label>Tgl Max Pinjam</label>
-                    <input type="date" name="tgl_max_pinjam"class="form-control" value="{{ $transaksi->tgl_max_pinjam }}" disabled>
-                    @error('tgl_max_pinjam')
-                    <small class="text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label>Tanggal Kembali</label>
-                    <input type="date" name="tgl_kembali"class="form-control" value="{{ date('Y-m-d') }}" disabled>
-                    @error('tgl_kembali')
-                    <small class="text-danger">{{ $message }}</small>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label>Keterangan</label>
-                    <textarea  name="ket"class="form-control" placeholder="optional">{{ $transaksi->ket }}</textarea>
-                </div>
+    @csrf
+    @method('put')
+    <div class="form-group">
+        <label>No Anggota</label>
+        <input type="text" placeholder="masukkan no anggota" name="nim" class="form-control" autocomplete="off" value="{{ $transaksi->anggota->nim }}" disabled>
+        @error('nim')
+            <small class="text-danger">{{ $message }}</small>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label>Buku</label>
+        <input type="text" name="judul" class="form-control" autocomplete="off" value="{{ $transaksi->buku->judul }}" disabled>
+        @error('buku_id')
+            <small class="text-danger">{{ $message }}</small>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label>Tgl Max Pinjam</label>
+        <input type="date" name="tgl_max_pinjam" class="form-control" value="{{ $transaksi->tgl_max_pinjam }}" disabled>
+        @error('tgl_max_pinjam')
+            <small class="text-danger">{{ $message }}</small>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label>Tanggal Kembali</label>
+        <input type="date" name="tgl_kembali" class="form-control" value="{{ date('Y-m-d') }}" disabled>
+        @error('tgl_kembali')
+            <small class="text-danger">{{ $message }}</small>
+        @enderror
+    </div>
+    <div class="form-group">
+        <label>Keterangan</label>
+        <textarea name="ket" class="form-control" placeholder="optional">{{ $transaksi->ket }}</textarea>
+    </div>
 
-                <div class="float-right">
-
-                    <button type="submit" class="btn btn-primary">Kembalikan</button>
-                </div>
-                </form>
-
-
+    <div class="float-right">
+        <button type="submit" class="btn btn-primary">Kembalikan</button>
+    </div>
+</form>
